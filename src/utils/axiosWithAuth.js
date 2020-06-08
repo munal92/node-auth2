413a1245c20b2309f98481ce252ccbc0de5726a5
unknown <m_fattih@hotmail.com>
2020-06-08 11:14:23 -0700

    deployed

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,17 @@
+
+import axios from 'axios';
+
+const axiosWithAuth = () => {
+    const token = window.localStorage.getItem("token")
+    return  axios.create(
+      {
+          headers:{
+              authorization:token 
+          },
+          baseURL:'http://localhost:5000'
+      }
+        
+    );
+};
+
+export default axiosWithAuth;
